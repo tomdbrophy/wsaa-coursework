1894e17319d8bd4607b9eee546de9a3a9ce47c9a
Tom Brophy <tombrophy@mac.com>
2024-02-16 15:01:51 +0000

    Current Weather Assignment updated

--- assignments/currentweather.py
+++ assignments/currentweather.py
@@ -5,3 +5,20 @@
 # I have set the lat/long to my location, you may use that or a different location.
 # Last few marks, also print out wind direction (10m)
 
+import requests
+import json
+
+url = 'https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m'
+response = requests.get(url)
+#print(response.json())
+
+data = response.json()
+
+current = data['current']
+#print(current)
+temperature = current['temperature_2m']
+
+units = data['current_units']
+degrees = units['temperature_2m']
+
+print(temperature,degrees)
