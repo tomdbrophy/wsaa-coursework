7f1c64c499b568210de6a2555cb6e2c8771961f0
Tom Brophy <tombrophy@mac.com>
2024-02-16 14:28:44 +0000

    Created weather / testing git on work PC

---
+++ assignments/currentweather.py
@@ -0,0 +1,7 @@
+# Assignment:
+# Using the URL below
+# https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m
+# Write a python program called currentweather.py that will print out the current temperature on the console (and only the temperature)
+# I have set the lat/long to my location, you may use that or a different location.
+# Last few marks, also print out wind direction (10m)
+
