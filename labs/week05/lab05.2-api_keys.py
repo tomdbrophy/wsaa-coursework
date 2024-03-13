88da14deea5880cb3370ada6b8d6552e5010ce5a
tomdbrophy <tombrophy@mac.com>
2024-03-13 21:18:56 +0000

    key removed

--- labs/week05/lab05.2-api_keys.py
+++ labs/week05/lab05.2-api_keys.py
@@ -3,7 +3,7 @@ import urllib.parse
 
 target_url = 'https://www.wikipedia.org/'
 
-api_key = 'RejLoxF2A892xMNg8wR8wMdjQqPB8EF9X2N0rYllPaeTVFx6WfyPl9soZi4v7Jxv'
+api_key = ''
 
 api_url = 'https://api.html2pdf.app/v1/generate'
 '''
@@ -12,8 +12,7 @@ parsed_params = urllib.parse.urlencode(params)
 request_url = api_url+'?'+parsed_params
 '''
 
-request_url = 'https://api.html2pdf.app/v1/generate?https://www.wikipedia.org/&apiKey={RejLoxF2A892xMNg8wR8wMdjQqPB8EF9X2N0rYllPaeTVFx6WfyPl9soZi4v7Jxv}'
-
+request_url = ''
 
 response = requests.get(request_url)
 print(response.status_code)
